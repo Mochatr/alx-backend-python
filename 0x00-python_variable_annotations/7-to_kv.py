5379aa6f9cdac88cf015fbefbc4d692d0c283208
Mochatr <Mochatr>
2024-05-04 19:55:59 +0000

    Update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> float:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple containing the string
     and the square of the numeric value as a float.
