d1b082246b50b53a84c5a815bf31d539a1b578f1
Mochatr <Mochatr>
2024-05-04 18:34:27 +0000

    String representation of a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Define to_str function"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the float
+
+    Args:
+      n (float): float
+
+    Returns:
+      str: String representation of the float
+    """
+    return str(n)
