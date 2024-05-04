aaa4264ea59b15559d243c71284be8ba3e049a62
Mochatr <Mochatr>
2024-05-04 17:56:10 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Define add function"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two float numbers
+
+    Args:
+        a (float): float
+        b (float): float
+
+    Returns:
+        The sum as float
+    """
+    return a + b
