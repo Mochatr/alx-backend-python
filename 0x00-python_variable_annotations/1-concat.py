7c71aaf638cbb37c5d2e9bb56058d4880f235d09
Mochatr <Mochatr>
2024-05-04 18:10:04 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Define concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns a concatenated string
+
+    Args:
+      str1(string): First string
+      str2(string): Second string
+
+    Returns:
+      A concatenated string
+    """
+    return str1 + str2
