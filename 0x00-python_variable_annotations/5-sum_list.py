359cfb2cba63a119b0c35de26dd466cb2361a565
Mochatr <Mochatr>
2024-05-04 19:05:56 +0000

    List of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Define sum_list function"""
+
+from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Returns the sum of a list of float.
+
+    Args:
+      input_list (list[float]): list of float numbers
+
+    Returns:
+      float: the sum of a list of float.
+    """
+    return sum(input_list)
