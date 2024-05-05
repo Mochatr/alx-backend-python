16ba4a9c9939b20c492b536b0e5acab68ead5f86
Mochatr <Mochatr>
2024-05-05 23:51:52 +0000

    Update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,24 +4,21 @@
 from typing import TypeVar, Dict, Optional
 
 
-K = TypeVar('K')
-V = TypeVar('V')
+K = TypeVar('K')  # Generic type for the key
+V = TypeVar('V')  # Generic type for the value
 
 
 def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> Optional[V]:
     """
-    Return the value associated with the key in the dictionary
-    or the default value.
+    Retrieve a value from a dictionary by key with a fallback to a default value.
 
     Args:
-      dct (Dict[K, V]): Dictionary
-      key (K): the key in the dictionary
-      default (Optional[V]): The default value to return
-      if the key is not found.
+    dct (Dict[K, V]): The dictionary from which to retrieve the value.
+    key (K): The key to look up in the dictionary.
+    default (Optional[V]): The default value to return if the key is not found.
 
     Returns:
-      the value associated with the key in the dictionary
-      or the default value.
+    Optional[V]: The value associated with the key in the dictionary, or the default value.
     """
     if key in dct:
         return dct[key]
