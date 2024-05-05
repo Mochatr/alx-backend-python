06bfb1256d871362f1d8eff1d98cabc67ebe01bd
Mochatr <Mochatr>
2024-05-05 23:46:08 +0000

    More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Define safely_get_value function"""
+
+from typing import Dict, TypeVar, Optional
+
+
+K = TypeVar('K')
+V = TypeVar('V')
+
+
+def safely_get_value(dct: Dict[k, v], key: K, default: Optional[V] = None) -> Optional[V]:
+    """
+    Return the value associated with the key in the dictionary
+    or the default value.
+
+    Args:
+      dct (Dict[K, V]): Dictionary
+      key (K): the key in the dictionary
+      default (Optional[V]): The default value to return
+      if the key is not found.
+
+    Returns:
+      the value associated with the key in the dictionary
+      or the default value.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
