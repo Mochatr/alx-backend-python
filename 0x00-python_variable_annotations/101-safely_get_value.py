bb2d51e2c05e449a88b0ed75da9dbf655f76dcab
Mochatr <Mochatr>
2024-05-06 01:04:20 +0000

    Update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 """Define safely_get_value function"""
 
-from typing import TypeVar, Dict, Optional
+from typing import Mapping, Any, TypeVar, Union, Optional
 
 
-K = TypeVar('K')  # Generic type for the key
-V = TypeVar('V')  # Generic type for the value
+T = TypeVar('T')
+ret_t = Union[Any, T]
+def_t = Optional[T]
 
 
-def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> Optional[V]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: def_t = None) -> ret_t:
     """
-    Retrieve a value from a dictionary by key with a fallback to a default value.
+    Retrieve a value from a dictionary
+    by key with a fallback to a default value.
 
     Args:
-    dct (Dict[K, V]): The dictionary from which to retrieve the value.
-    key (K): The key to look up in the dictionary.
-    default (Optional[V]): The default value to return if the key is not found.
+    dct : The dictionary from which to retrieve the value.
+    key : The key to look up in the dictionary.
+    default : The default value to return if the key is not found.
 
     Returns:
-    Optional[V]: The value associated with the key in the dictionary, or the default value.
+      The value associated with the key in the dictionary,
+      or the default value.
     """
     if key in dct:
         return dct[key]
