5803a6627bf7dfbcd69ab43b0becfcdee5befcb5
Mochatr <Mochatr>
2024-05-06 17:36:20 +0000

    Measure runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Use the necessary modules """
+
+import asyncio
+import time
+from typing import float
+
+wait_n = __import__("1-concurrent_coroutines.py").wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the average time per execution of wait_n
+    called with n and max_delay.
+
+    Args:
+      n (int): Number of times wait_random should be spawned.
+      max_delay (int): Maximum delay that wait_random can use.
+
+    Returns:
+      float: The average time per execution.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
+
+
+if __name__ == "__main__":
+    n = 10
+    max_delay = 10
+    average_time = measure_time(n, max_delay)
+    print(f"Average execution time: {average_time:.2f} seconds")
