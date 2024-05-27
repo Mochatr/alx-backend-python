d360a7180ab6145dc63c4c016137238745e124fa
Mochatr <mohamedchatr8@gmail.com>
2024-05-27 20:42:05 +0000

    fix: indentation

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -58,17 +58,17 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with
             ("https://api.github.com/orgs/test_org/repos")
 
-        @parameterized.expand([
-            ({"license": {"key": "my_license"}}, "my_license", True),
-            ({"license": {"key": "other_license"}}, "my_license", False)
-        ])
-        def test_has_license(self, repo, license_key, expected):
-            """
-            Test GithbOrgClient.has_licence returns
-            the correct result.
-            """
-            result = GithubOrgClient.has_license(repo, license_key)
-            self.assertEqual(result, expected)
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test GithbOrgClient.has_licence returns
+        the correct result.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
