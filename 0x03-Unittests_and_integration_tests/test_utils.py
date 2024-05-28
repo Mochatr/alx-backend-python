838294ba662ac9ace33a09ee83a5720a571b4784
Mochatr <mohamedchatr8@gmail.com>
2024-05-28 13:36:35 +0000

    update Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,30 +3,38 @@
 
 
 import unittest
+from parameterized import parameterized
+from utils import access_nested_map
 from unittest.mock import patch
 from utils import memoize
 
 
-class TestMemoize(unittest.TestCase):
-    """Test class for memoize decorator"""
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+        
+        class TestMemoize(unittest.TestCase):
+            """Test class for memoize decorator"""
+            def test_memoize(self):
+                """Test memoize decorator caches results properly"""
+                class TestClass:
+                    def a_method(self):
+                        return 42
 
-    def test_memoize(self):
-        """Test memoize decorator caches results properly"""
-
-        class TestClass:
-            def a_method(self):
-                return 42
-
-            @memoize
-            def a_property(self):
-                return self.a_method()
-
-        with patch.object(TestClass, 'a_method',
-                          return_value=42) as mock_method:
-            test_obj = TestClass()
-            self.assertEqual(test_obj.a_property, 42)
-            self.assertEqual(test_obj.a_property, 42)
-            mock_method.assert_called_once()
+                    @memoize
+                    def a_property(self):
+                        return self.a_method()
+                with patch.object(TestClass, 'a_method',
+                                  return_value=42) as mock_method:
+                    test_obj = TestClass()
+                    self.assertEqual(test_obj.a_property, 42)
+                    self.assertEqual(test_obj.a_property, 42)
+                    mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
